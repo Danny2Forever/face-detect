048b70859edf86c9a01c64ba92bd59cf38cda7e7
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2022-11-20 19:09:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
 import os
+from skimage import io
 
 face_cascade = cv2.CascadeClassifier(os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml'))
 eye_cascade = cv2.CascadeClassifier(os.path.join(cv2.data.haarcascades, 'haarcascade_eye.xml'))
 
-img = cv2.imread('images/test_img.jpg')
+img = io.imread('face_detect/images/test_img.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
